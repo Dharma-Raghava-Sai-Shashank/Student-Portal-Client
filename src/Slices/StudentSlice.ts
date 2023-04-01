8b3f0360415abb5b8295b742520262515a322809
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2023-04-01 17:17:32 +0530

    reduxsetup

---
+++ src/Slices/StudentSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+export const studentSlice  =  createSlice({
+    name  : "students",
+    initialState,
+    reducers:{
+     
+       
+    }
+})
+export const {} =studentSlice.actions
+export default studentSlice.reducer
