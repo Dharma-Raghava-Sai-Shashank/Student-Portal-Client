6e6af634c9833053541b57f8abe121656cba10e3
Niket Gupta <niketgupta101@gmail.com>
2023-04-24 22:52:07 +0530

    Sidebar stylling

--- src/Student/Sidebars/StudentSidebar.tsx
+++ src/Student/Sidebars/StudentSidebar.tsx
@@ -1,34 +1,31 @@
 import React, { useState, useRef } from "react";
 import MotionPhotosAutoIcon from "@mui/icons-material/MotionPhotosAuto";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import GradingIcon from "@mui/icons-material/Grading";
 import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import HomeIcon from "@mui/icons-material/Home";
-import MenuIcon from "@mui/icons-material/Menu";
 import ViewCompactIcon from "@mui/icons-material/ViewCompact";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import { Link } from "react-router-dom";
 import "./index.scss";
-
-// interface props {
-//   option: string;
-//   setOption: React.Dispatch<React.SetStateAction<string>>;
-//   session: string;
-//   setSession: React.Dispatch<React.SetStateAction<string>>;
-// }
+import { Box } from "@mui/material";
 
 export const StudentSidebar = () => {
-  const windowSize = useRef([window.innerWidth, window.innerHeight]);
-  // console.log(windowSize.current[0]);
+  const [width, setWidth] = React.useState(window.innerWidth);
 
-  const [collapse, setCollapse] = useState(
-    windowSize.current[0] < 720 ? true : false
-  );
+  const resizeWindow = () => {
+    setWidth(window.innerWidth);
+  }
+
+  React.useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, [])
 
   return (
-    <div>
-      <div className="d-flex h-100">
-        {collapse === false ? (
+    <>
+      {width>=700 ? (
+        <Box className="d-flex">
           <div className="d-flex">
             <div className="MainSidebar">
               <div className="topLogo d-flex justify-content-between">
@@ -38,14 +35,6 @@ export const StudentSidebar = () => {
                   </div>
                   <div className="LogoName">Autometa</div>
                 </div>
-                <button
-                  className="closeButton"
-                  onClick={() => {
-                    setCollapse(true);
-                  }}
-                >
-                  <CloseOutlinedIcon fontSize="small" />
-                </button>
               </div>
 
               <div className="MainSidebarOptions my-5">
@@ -95,72 +84,105 @@ export const StudentSidebar = () => {
               </div>
             </div>
           </div>
-        ) : (
-          <div className="topLogoHamburger responsiveSidebar px-0 py-3">
-            <div className="MainSidebar" style={{ width: "inherit" }}>
-              <div className="d-flex justify-content-center">
-                <div className="logo">
-                  <MotionPhotosAutoIcon fontSize="large" />
-                </div>
-              </div>
-              <div className="d-flex justify-content-center">
-                <button
-                  className="closeButton"
-                  onClick={() => setCollapse(false)}
+        </Box>
+      ) : (
+        <Box
+          className="d-flex"
+          sx={{
+            position: "fixed",
+            width: "100%",
+            minHeight: "5vh",
+            bottom: 0,
+            left: 0,
+            boxSizing: "border-box",
+          }}
+        >
+          <div
+            className="topLogoHamburger responsiveSidebar"
+            style={{ width: "100vw", height: "70px", padding: 0, zIndex: 99 }}
+          >
+            <div
+              className="MainSidebar"
+              style={{ width: "inherit", display: "flex", minHeight: "70px" }}
+            >
+              <div
+                className="MainSidebarOption"
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  width: "100%",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ height: "60px", width: "60px", display: "inline" }}
                 >
-                  <MenuIcon fontSize="medium" />
-                </button>
-              </div>
-              <div className="topLogo p-0">
-                <div className="MainSidebarOptions my-4">
-                  <div className="MainSidebarOption">
-                    <div className="d-flex justify-content-center">
-                      <Link to="/student/dashboard">
-                        <button
-                          className="text-white my-3"
-                          style={{ backgroundColor: "inherit", border: "none" }}
-                        >
-                          <HomeIcon fontSize="large" />
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                      <Link to="/student/profile">
-                        <button
-                          className="text-white my-3"
-                          style={{ backgroundColor: "inherit", border: "none" }}
-                        >
-                          <PermIdentityOutlinedIcon fontSize="large" />
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                      <Link to="/student/jobprofile">
-                        <button
-                          className="text-white my-3"
-                          style={{ backgroundColor: "inherit", border: "none" }}
-                        >
-                          <WorkOutlineIcon fontSize="large" />
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="extras text-center ">
-                      <Link to="/student/extras">
-                        <button
-                          className="text-white my-3"
-                          style={{ backgroundColor: "inherit", border: "none" }}
-                        >
-                          <ViewCompactIcon fontSize="large" />
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
+                  <Link to="/student/dashboard">
+                    <button
+                      className="text-white my-3"
+                      style={{
+                        backgroundColor: "inherit",
+                        border: "none",
+                      }}
+                    >
+                      <HomeIcon fontSize="large" />
+                    </button>
+                  </Link>
+                </div>
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ height: "60px", width: "60px", display: "inline" }}
+                >
+                  <Link to="/student/profile">
+                    <button
+                      className="text-white my-3"
+                      style={{
+                        backgroundColor: "inherit",
+                        border: "none",
+                      }}
+                    >
+                      <PermIdentityOutlinedIcon fontSize="large" />
+                    </button>
+                  </Link>
+                </div>
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ height: "60px", width: "60px", display: "inline" }}
+                >
+                  <Link to="/student/jobprofile">
+                    <button
+                      className="text-white my-3"
+                      style={{
+                        backgroundColor: "inherit",
+                        border: "none",
+                      }}
+                    >
+                      <WorkOutlineIcon fontSize="large" />
+                    </button>
+                  </Link>
+                </div>
+                <div
+                  className="extras text-center "
+                  style={{ height: "60px", width: "60px", display: "inline" }}
+                >
+                  <Link to="/student/extras">
+                    <button
+                      className="text-white my-3"
+                      style={{
+                        backgroundColor: "inherit",
+                        border: "none",
+                      }}
+                    >
+                      <ViewCompactIcon fontSize="large" />
+                    </button>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
-        )}
-      </div>
-    </div>
+        </Box>
+      )}
+    </>
   );
 };
--- src/api/job.service.tsx
+++ src/api/job.service.tsx
@@ -1,16 +1,28 @@
 import { APIRequest, baseURL } from ".";
 
-const studentURL = baseURL + '/jobs';
-const adminBaseURL = studentURL + '/admin';
+const studentURL = baseURL + "/jobs";
+const adminBaseURL = studentURL + "/admin";
 
 export const fetchJobsForAdmin = async (placementCycleId: number) =>
-await APIRequest(adminBaseURL + `/${placementCycleId}`, "GET");
+  await APIRequest(adminBaseURL + `/${placementCycleId}`, "GET");
 
-export const searchJobsForAdmin = async (placementCycleId: number, query: string) =>
-await APIRequest(adminBaseURL + `/search/${placementCycleId}/${query}`, "GET");
+export const searchJobsForAdmin = async (
+  placementCycleId: number,
+  query: string
+) =>
+  await APIRequest(
+    adminBaseURL + `/search/${placementCycleId}/${query}`,
+    "GET"
+  );
 
 export const deleteJob = async (jobId: number) =>
-await APIRequest(adminBaseURL + `/${jobId}`, "DELETE");
+  await APIRequest(adminBaseURL + `/${jobId}`, "DELETE");
 
 export const createJob = async (job: any) =>
-await APIRequest(adminBaseURL + `/`, "POST", job);
+  await APIRequest(adminBaseURL + `/`, "POST", job);
+
+export const checkEligibilityForJob = async (admno: string, nfId: number) =>
+  await APIRequest(studentURL + "/check/eligible", "PUT", { admno, nfId });
+
+export const applyForJobByAdmin = async (admno: string, nfId: number) =>
+  await APIRequest(adminBaseURL + "/check/apply", "PUT", { admno, nfId });
