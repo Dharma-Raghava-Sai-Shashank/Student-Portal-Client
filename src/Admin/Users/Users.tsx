658a082ec160ef0941f8f79dab3cf9e9ec1c3dac
memyselfandglitch <deveshisingh7b@gmail.com>
2023-07-20 23:25:38 +0530

    Users Page Table

--- src/Admin/Users/Users.tsx
+++ src/Admin/Users/Users.tsx
@@ -1,41 +1,26 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState } from 'react';
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
 import { Autocomplete } from "@mui/material";
-import ChatIcon from "@mui/icons-material/Chat";
-import { Card, CardContent, Typography, Avatar, Grid } from "@mui/material";
+import { Card, CardContent, Typography, Avatar, Grid, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import SettingsIcon from "@mui/icons-material/Settings";
 
 const users = [
-  { name: 'Deveshi Singh', admissionNumber: '21je0304' },
-  { name: 'Deepika', admissionNumber: '21je0900' },
-  { name: 'Tanya', admissionNumber: '20je0294' },
-  { name: 'Harsh', admissionNumber: '19je0404' },
-  { name: 'Abc', admissionNumber: '19je0404' }
+  { name: 'Deveshi Singh', admissionNumber: '21je0304', lastName: 'xyz', email: '21je0304@iitism.ac.in', company: 'None' },
+  { name: 'Deepika', admissionNumber: '21je0900', lastName: 'Abc', email: 'xyz@gmail.com', company: 'TCS' }
 ];
 
 export const Users = () => {
-
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-sm ">
+        <div className="col-4">
           <br></br>
           <h4>Search for users</h4>
           <br></br>
           <div className="search-box">
-          <Autocomplete
+            <Autocomplete
               options={users}
               getOptionLabel={(option) => option.name}
               renderInput={(params) => (
@@ -58,8 +43,8 @@ export const Users = () => {
           </div>
           {users.map((user, index) => (
             <Card key={index} >
-              <CardContent style={{ height: '100px' }}>
-                <Grid container spacing={2} alignItems="center" >
+              <CardContent style={{ height: '75px'}}>
+                <Grid container spacing={2} alignItems="center">
                   <Grid item>
                     <Avatar>
                       <PersonIcon />
@@ -68,10 +53,10 @@ export const Users = () => {
                   <Grid item xs={12} sm container>
                     <Grid item xs container direction="column" spacing={1}>
                       <Grid item xs>
-                        <Typography variant="h6">{user.name}</Typography>
+                        <Typography sx={{ fontSize: 14 }} gutterBottom>{user.name}</Typography>
                       </Grid>
                       <Grid item xs>
-                        <Typography variant="body1" color="textSecondary">
+                        <Typography sx={{ fontSize: 14 }} variant="body1" color="textSecondary">
                           Admission Number: {user.admissionNumber}
                         </Typography>
                       </Grid>
@@ -82,10 +67,56 @@ export const Users = () => {
             </Card>
           ))}
         </div>
-        <div className="col-sm "></div>
-        <div className="col-sm "></div>
+        <div className="col-8">
+          <div className="roles-section">
+            <br></br>
+            <Typography variant="h6">Roles: </Typography>
+            <br></br>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Button variant="contained" color="info">
+                  Admin
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="info">
+                  Agent
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="info">
+                  Customer
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+          <br></br>
+          <div className="table-container">
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow style={{ background: '#f0f0f0' }}>
+                    <TableCell>Email</TableCell>
+                    <TableCell>First Name</TableCell>
+                    <TableCell>Last Name</TableCell>
+                    <TableCell>Company</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {users.map((user, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.name}</TableCell>
+                      <TableCell>{user.lastName}</TableCell>
+                      <TableCell>{user.company}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
